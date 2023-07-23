47072c53e0d8783c20cbc916a3782f683784a973
Sena Lim <tpsk8322@naver.com>
2023-07-23 14:36:27 +0900

    refactor: 회원가입 요청 시 사용자 이름 속성명 변경(username -> name)

--- src/lib/api/auth.js
+++ src/lib/api/auth.js
@@ -1,7 +1,7 @@
 import client from "./client";
 
-export const signUp = ({email, password, checkPassword, username, phoneNumber, referenceUrl, part, organization, motto}) =>
-    client.post("/api/auth/signUp", ({email, password, checkPassword, username, phoneNumber, referenceUrl, part, organization, motto}));
+export const signUp = ({email, password, checkPassword, name, phoneNumber, referenceUrl, part, organization, motto}) =>
+    client.post("/api/auth/signUp", ({email, password, checkPassword, name, phoneNumber, referenceUrl, part, organization, motto}));
 
 export const login = ({email, password}) =>
     client.post("/api/auth/login", ({email, password}));
