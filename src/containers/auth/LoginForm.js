5e562323f379b0b951bb479dfc330857d269d9a2
Sena Lim <tpsk8322@naver.com>
2023-07-23 14:54:51 +0900

    feat: axios interceptor 적용

--- src/containers/auth/LoginForm.js
+++ src/containers/auth/LoginForm.js
@@ -2,11 +2,14 @@ import AuthForm from "../../components/auth/AuthForm";
 import {useDispatch, useSelector} from "react-redux";
 import {changeField, initializeForm, login} from "../../modules/auth";
 import {useEffect} from "react";
+import client from "../../lib/api/client";
 
 const LoginForm = () => {
     const dispatch = useDispatch();
-    const {form} = useSelector(({auth}) => ({
+    const {form, auth, authError} = useSelector(({auth}) => ({
         form: auth.login,
+        auth: auth.auth,
+        authError: auth.authError,
     }));
 
     const onChange = e => {
@@ -31,6 +34,22 @@ const LoginForm = () => {
     useEffect(() => {
         dispatch(initializeForm('login'));
     }, [dispatch]);
+
+    useEffect(() => {
+        if (auth) {
+            try {
+                localStorage.setItem('member', JSON.stringify(auth));
+                client.defaults.headers.common['Authorization'] = `Bearer ${auth}`;
+            } catch (e) {
+                console.log('localStorage is not working.');
+            }
+        }
+        if (authError) {
+            console.log(authError);
+            console.log('로그인 실패');
+            return;
+        }
+    }, [auth, authError]);
 
     return (
         <AuthForm type="login"
