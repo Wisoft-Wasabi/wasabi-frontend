eef626d4c4d8f3b43bd8158bbd309e3a8dafb311
Sena Lim <tpsk8322@naver.com>
2023-08-22 17:10:49 +0900

    feat: 개인정보 조회

--- src/modules/mypage.js
+++ src/modules/mypage.js
@@ -5,19 +5,31 @@ import * as myPageAPI from "../lib/api/mypage";
 
 const [MY_BOARD_LIST, MY_BOARD_LIST_SUCCESS, MY_BOARD_LIST_FAILURE] = createRequestActionTypes('myPage/MY_BOARD_LIST');
 const [MY_LIKE_LIST, MY_LIKE_LIST_SUCCESS, MY_LIKE_LIST_FAILURE] = createRequestActionTypes('myPage/MY_LIKE_LIST');
+const [MY_PROFILE, MY_PROFILE_SUCCESS, MY_PROFILE_FAILURE] = createRequestActionTypes('myPage/MY_PROFILE');
 
 export const myBoardList = createAction(MY_BOARD_LIST);
 export const myLikeList = createAction(MY_LIKE_LIST);
+export const myProfile = createAction(MY_PROFILE);
 
 // 사가 생성
 const myBoardListSaga = createRequestSaga(MY_BOARD_LIST, myPageAPI.myBoardList);
 const myLikeListSaga = createRequestSaga(MY_LIKE_LIST, myPageAPI.myLikeList);
+const myProfileSaga = createRequestSaga(MY_PROFILE, myPageAPI.myProfile);
 export function* myPageSaga() {
     yield takeLatest(MY_BOARD_LIST, myBoardListSaga);
     yield takeLatest(MY_LIKE_LIST, myLikeListSaga);
+    yield takeLatest(MY_PROFILE, myProfileSaga);
 }
 
 const initialState = {
+    profile: {
+        name: '',
+        phoneNumber: '',
+        referenceUrl: '',
+        part: '',
+        organization: '',
+        motto: '',
+    },
     my: null,
     myError: null,
 };
@@ -37,6 +49,14 @@ const myPage = handleActions(
             my,
         }),
         [MY_LIKE_LIST_FAILURE]: (state, {payload: error}) => ({
+            ...state,
+            myError: error,
+        }),
+        [MY_PROFILE_SUCCESS]: (state, {payload: my}) => ({
+            ...state,
+            my,
+        }),
+        [MY_PROFILE_FAILURE]: (state, {payload: error}) => ({
             ...state,
             myError: error,
         }),
