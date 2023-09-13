a59deae606b70cb10ebc6c8bbbde79c04c219a6e
Sena Lim <tpsk8322@naver.com>
2023-09-13 16:03:15 +0900

    feat: 회원가입 요청 승인

--- src/lib/api/admin.js
+++ src/lib/api/admin.js
@@ -1,3 +1,5 @@
 import client from "./client";
 
 export const readSignUpList = () => client.get('/api/admin/members');
+
+export const acceptSignUpRequest = ({memberId}) => client.patch('/api/admin/members', ({memberId}));
