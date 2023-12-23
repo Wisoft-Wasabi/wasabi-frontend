7788860cb993018cb1182574d698c645c94e6a25
Sena Lim <tpsk8322@naver.com>
2023-12-23 21:55:12 +0900

    style: tawilwindcss를 사용하여 CSS 팔레트 설정

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js}'],
+  theme: {
+    extend: {
+      colors: {
+        brand: '#61C08E',
+        brandAccent: '#50A478',
+        text1: '#101210',
+        text2: '#383A38',
+        text3: '#656665',
+        text4: '#959795',
+        buttonText: '#fff',
+        border1: '#242424',
+        border2: '#AFAFAF',
+        border3: '#D3D3D3',
+        border4: '#ECECEC',
+        background1: '#FAFAFA',
+        background2: '#fff'
+      }
+    },
+  },
+  plugins: [],
+}
