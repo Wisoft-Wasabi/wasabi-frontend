72e386751b00459c43202f68f48fcc5fb1abfa0d
MinSeok Choi <100674871+Choiminseok18@users.noreply.github.com>
2024-01-08 21:00:29 +0900

    style: UI 수정

--- src/components/common/BoardList.js
+++ src/components/common/BoardList.js
@@ -1,33 +1,30 @@
 import ThumbNail from "../../assets/thumbnail.jpg";
 import {AiOutlineHeart} from "react-icons/ai";
 import {useNavigate} from "react-router-dom";
-import {GrLineChart} from "react-icons/gr";
-import { GoPencil } from "react-icons/go";
-import InfiniteScroll from "./InfiniteScroll";
+import { FaRegEye } from "react-icons/fa";
+
 
 const BoardCard = ({board, onNavigateDetail}) => {
-    const {title, writer, createdAt, likeCount, views} = board;
-    const body = '내용이 들어갈 부분입니다. 60자가 넘어가면 뒷부분이\'...\'으로 표시가 되도록 만들었습니다. 너무 짧다면 늘릴 것이고 너무 길다면 줄일 것입니다. 이곳에 표시할 내용은 아직 받아오지 못하고 있으므로 보시는 것과 같은 임시 데이터가 들어가 있습니다.'
+    const {title, writer, createdAt, likeCount, views, body} = board;
 
     return (
       <div className='flex flex-col w-80 h-96 border-none rounded-lg bg-white shadow-lg m-4 overflow-hidden hover:cursor-pointer hover:-translate-y-3 hover:transition-transform hover:ease-in-out duration-300' onClick={onNavigateDetail}>
           <img className='w-full h-44' src={ThumbNail} alt='ThumbNail'/>
           <div className='flex flex-col border px-5 py-5'>
-          {title.length > 16 ? <h2 className='px-0 pb-3 text-xl font-medium text-text1'>{[...title].slice(0, 15)}...</h2> : <h2 className='m-0 mb-7 text-2xl font-medium text-text1'>{title}</h2> }
-          {body.length > 80 ? <p className='text-xs text-text3'>{[...body].slice(0, 79)}...</p> : <p>{body}</p>}
+          {title.length > 16 ? <h2 className='px-0 pb-3 text-xl font-bold text-text1'>{[...title].slice(0, 15)}...</h2> : <h2 className='px-0 pb-3 text-xl font-bold text-text1'>{title}</h2> }
+          {body.length > 89 ? <p className='text-xs text-text3'>{[...body].slice(0, 88)}...</p> : <p className='text-xs text-text3'>{body}</p>}
             <div className='flex flex-row text-sm pt-3 text-text1'>
-              <GoPencil className='h-5' />
-              {writer.length > 18 ? <p className='px-1 font-medium'>{[...writer].slice(0, 17)}...</p> : <p>{writer}</p>}
+              {writer.length > 18 ? <p className='px-1 font-medium text-text2'>by. {[...writer].slice(0, 17)}...</p> : <p className='px-1 font-medium text-text2'>by. {writer}</p>}
             </div>
       </div>
-          <div className='flex flex-row py-2 px-4 space-x-28'>
+          <div className='flex flex-row py-2 px-4 space-x-24'>
             <div className='flex flex-row'>
               <p className='flex flex-row items-center text-lg pl-2'>
                     <AiOutlineHeart/>
                     <p className='pl-2'>{likeCount}</p>
                 </p>
-              <p className='flex flex-row items-center text-lg pl-2'>
-                    <GrLineChart/>
+              <p className='flex flex-row items-center text-lg pl-3'>
+                    <FaRegEye/>
                     <p className='pl-2'>{views}</p>
                 </p>
             </div>
@@ -49,7 +46,7 @@ const BoardList = ({boards, loading}) => {
           {!loading && boards && (
           <>
                   {boards.length !== 0 ?
-                  <div className='grid grid-cols-3 grid-rows-2 gap-12 mt-12'>
+                  <div className='xl:grid xl:grid-cols-3 xl:grid-rows-2 xl:gap-12 xl:mt-12  md:grid md:grid-cols-2 md:grid-rows-2 md:gap-12 md:mt-12  grid grid-cols-1 gap-12 mt-12'>
                       {boards.map(board => (
                       <BoardCard
                         board={board}
