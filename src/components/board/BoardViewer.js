e4ec12ed333f3e05069c978548a94adff579466f
Sena Lim <tpsk8322@naver.com>
2024-01-15 05:27:30 +0900

    feat: 댓글 조회 및 UI

--- src/components/board/BoardViewer.js
+++ src/components/board/BoardViewer.js
@@ -2,32 +2,34 @@ import styled from "styled-components";
 import {BiShow} from "react-icons/bi";
 import WriterViewer from "./WriterViewer";
 import {useState} from "react";
+import CommentViewer from "../comment/CommentViewer";
 
 const BoardViewerBlock = styled.div`
     margin-top: 4rem;
 `;
 
 const BoardHead = styled.div`
-  border-bottom: 1px solid darkgrey;
-  padding-bottom: 2rem;
-  margin-bottom: 3rem;
+    border-bottom: 1px solid darkgrey;
+    padding-bottom: 2rem;
+    margin-bottom: 3rem;
 
-  h1 {
-    font-size: 3rem;
-    line-height: 1.5;
-    margin: 0;
-  }
+    h1 {
+        font-size: 3rem;
+        line-height: 1.5;
+        margin: 0;
+    }
 `;
 
 const SubInfo = styled.div`
-  margin-top: 1rem;
+    margin-top: 1rem;
 
-  /* span 사이에 가운뎃점 문자 넣기 */
-  span + span:before {
-    padding-left: 0.5rem;
-    padding-right: 0.5rem;
-    content: '\\B7'; /* 가운뎃점 문자 */
-  }
+    /* span 사이에 가운뎃점 문자 넣기 */
+
+    span + span:before {
+        padding-left: 0.5rem;
+        padding-right: 0.5rem;
+        content: '\\B7'; /* 가운뎃점 문자 */
+    }
 `;
 
 const BoardContent = styled.div`
@@ -36,17 +38,17 @@ const BoardContent = styled.div`
 
 const Tags = styled.div`
     margin-top: 0.5rem;
-  
-  .tag {
-    display: inline-block;
-    color: #4BC75F;
-    text-decoration: none;
-    margin-right: 0.5rem;
-    
-    &:hover {
-      color: #9FCF58;
+
+    .tag {
+        display: inline-block;
+        color: #4BC75F;
+        text-decoration: none;
+        margin-right: 0.5rem;
+
+        &:hover {
+            color: #9FCF58;
+        }
     }
-  }
 `;
 
 const BoardViewer = ({board, boardError, loading}) => {
@@ -65,26 +67,30 @@ const BoardViewer = ({board, boardError, loading}) => {
         return null;
     }
 
-    const {title, content, writer, createdAt, tag, views} = board.data;
+    const {title, content, writer, createdAt, tag, views, comments} = board.data;
 
     return (
-        <BoardViewerBlock>
-            <BoardHead>
-                <h1>{title}</h1>
-                <SubInfo>
-                    <span style={{cursor: "pointer"}} onClick={() => setIsOpenWriter(!isOpenWriter)}><b>{writer.name}</b></span>
-                    <span>{new Date(createdAt).toLocaleString()}</span>
-                    <span><BiShow/>{views}</span>
-                </SubInfo>
-                {isOpenWriter && <WriterViewer writer={writer}/>}
-            </BoardHead>
-            <BoardContent dangerouslySetInnerHTML={{__html: content}}/> {/* HTML 적용 */}
-            {tag && (
-                <Tags>
-                    <div className="tag">#{tag}</div>
-                </Tags>
-            )}
-        </BoardViewerBlock>
+        <>
+            <BoardViewerBlock>
+                <BoardHead>
+                    <h1>{title}</h1>
+                    <SubInfo>
+                        <span style={{cursor: "pointer"}}
+                              onClick={() => setIsOpenWriter(!isOpenWriter)}><b>{writer.name}</b></span>
+                        <span>{new Date(createdAt).toLocaleString()}</span>
+                        <span><BiShow/>{views}</span>
+                    </SubInfo>
+                    {isOpenWriter && <WriterViewer writer={writer}/>}
+                </BoardHead>
+                <BoardContent dangerouslySetInnerHTML={{__html: content}}/> {/* HTML 적용 */}
+                {tag && (
+                    <Tags>
+                        <div className="tag">#{tag}</div>
+                    </Tags>
+                )}
+            </BoardViewerBlock>
+            <CommentViewer comments={comments}/>
+        </>
     );
 };
 
